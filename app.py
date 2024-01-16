cfa3af567b0937eaea372caf797fc1db2f86650d
william-song-shy <songhongyishy@gmail.com>
2024-01-16 21:11:06 +0800

    rebuild

--- app.py
+++ app.py
@@ -1,225 +1,86 @@
-from apiflask import APIFlask, Schema, input, output
-from apiflask.fields import String, Integer, Float, Dict, List
-from apiflask.validators import Length, Range
 from koppen_climate import *
 import mapbox_geocoding
+from flask import Flask, jsonify, request
 
-app = APIFlask(__name__)
-
-
-@app.error_processor
-def my_error_handler(status_code, message, detail, headers):
-    return {
-        'status_code': status_code,
-        'message': message,
-        'detail': detail
-    }, status_code, {'Access-Control-Allow-Origin': '*'}
-
-
-class StationOut(Schema):
-    id = String()
-    name = String()
-    country = String()
-    lat = Float()
-    lon = Float()
-
-
-class StationFindIn(Schema):
-    name = String(required=True, validate=Length(1, 75))
-    length = Integer(required=False, missing=10, validate=Range(2, 15))
-
-
-class StationIn(Schema):
-    id = String(required=True)
-
-
-class PlaceIn (Schema):
-    name = String(required=True, validate=Length(1, 80))
-    limit = Integer(required=False, missing=5, validate=Range(1, 15))
-
-
-class PlaceOut (Schema):
-    zh_name = String()
-    en_name = String()
-    lat = Float()
-    lon = Float()
-    bbox = List(Float())
-
-
-class StationandClimateOut(Schema):
-    id = String()
-    name = String()
-    country = String()
-    lat = Float()
-    lon = Float()
-    data = List(Dict())
-    koppentype = String()
-    chinesetype = String()
-    source = String()
-
-
-class PointIn(Schema):
-    lat = Float(required=True, validate=Range(-90, 90))
-    lon = Float(required=True, validate=Range(-180, 180))
-
-
-class PointandClimateOut(Schema):
-    country = String()
-    nearby_stations = List(Dict())
-    data = List(Dict())
-    koppentype = String()
-    chinesetype = String()
-    source = String()
-
-
-@app.get('/')
-def main():
-    """Show some imformation
-
-    """
-    return {"about": "See /redoc to see how to use this API"}
+app = Flask(__name__)
 
 
 @app.get("/place/find")
-@input(PlaceIn, location="query")
-@output(PlaceOut(many=True))
-def FindPlace(data):
-    """Find a place
-    Find a place (city,country,village etc.) by the name given.
-    This API returns and accepts the name both in Chinese and English
-    """
-    return mapbox_geocoding.search(data['name'], data['limit']), {'Access-Control-Allow-Origin': '*'}
+def FindPlace():
+    data = request.args
+    return jsonify(mapbox_geocoding.search(data["name"], data["limit"])), {
+        "Access-Control-Allow-Origin": "*"
+    }
 
 
-@app.get('/station/find')
-@input(StationFindIn, location="query")
-@output(StationOut(many=True))
-def FindStation(data):
-    """Find a station by name
-
-    the meteostat will return 400 if the name is too short to search<br>
-    this API will return the id(in meteostat),name,country(ISO 3166-1 alpha-2) and latlon data
-    """
-    data = search_station(query=data['name'], length=data['length'])
-    l = list()
-    for i in data:
-        l.append(
+@app.get("/station/find")
+def FindStation():
+    data = request.args
+    data = search_station(query=data["name"], length=data["length"])
+    return jsonify(
+        [
             {
-                'id': i.get_id(),
-                'name': i.get_name(),
-                'country': i.get_country(),
-                'lat': i.get_place().get_lat(),
-                'lon': i.get_place().get_lon()
-            })
-    return l, {'Access-Control-Allow-Origin': '*'}
+                "id": i.get_id(),
+                "name": i.get_name(),
+                "country": i.get_country(),
+                "lat": i.get_place().latitude,
+                "lon": i.get_place().longitude,
+            }
+            for i in data
+        ]
+    ), {"Access-Control-Allow-Origin": "*"}
 
 
-@app.get('/station/climate')
-@input(StationIn, location="query")
-@output(StationandClimateOut)
-def StationClimate(data):
-    """Get the climate data
-
-    """
+@app.get("/station/climate")
+def StationClimate():
+    data = request.args
     station = Station()
-    station.set_by_id(data['id'])
-    data=station.get_climate_data()
-    climatetype = data.get_koppen()
-    koppen2chinese = {
-        'Af': "热带雨林",
-        'Am': "热带季风",
-        'Aw': "热带草原",
-        'As': "热带草原",
-        'BWk': "温带干旱",
-        'BWh': "热带沙漠",
-        'BSh': "热带半干旱",
-        'BSk': "温带半干旱",
-        'Csa': "地中海",
-        'Csb': "地中海",
-        'Csc': "地中海",
-        'Cfa': "亚热带湿润",
-        'Cwa': "亚热带湿润",
-        'Cwb': "温带海洋",
-        'Cfb': "温带海洋",
-        'Cfc': "温带海洋",
-        'Dsa': "温带大陆",
-        'Dsb': "温带大陆",
-        'Dsc': "亚寒带",
-        'Dsd': "亚寒带",
-        'Dwa': "温带大陆性湿润",
-        'Dwb': "温带大陆性湿润",
-        'Dwc': "亚寒带",
-        'Dwd': "亚寒带",
-        'Dfa': "温带大陆性湿润",
-        'Dfb': "温带大陆性湿润",
-        'Dfc': "亚寒带",
-        'Dfd': "亚寒带",
-        'ET': "苔原",
-        'EF': "冰原"
-    }
-    return {
-        'id': station.get_id(),
-        'name': station.get_name(),
-        'country': station.get_country(),
-        'lat': station.get_place().get_lat(),
-        'lon': station.get_place().get_lon(),
-        'data': data.get_data(),
-        'koppentype': climatetype,
-        'chinesetype': koppen2chinese.get(climatetype),
-        'source': data.get_meta().get('source')+" from {} to {}".format(data.get_meta().get('start'), data.get_meta().get('end'))
-    }, {'Access-Control-Allow-Origin': '*'}
+    station.set_by_id(data["id"])
+    data, meta = station.get_climate_data()
+    climatetype = data_to_koppen(data)
+    return jsonify(
+        {
+            "id": station.get_id(),
+            "name": station.get_name(),
+            "country": station.get_country(),
+            "lat": station.get_place().latitude,
+            "lon": station.get_place().longitude,
+            "data": data,
+            "koppentype": climatetype,
+            "chinesetype": koppen2chinese.get(climatetype),
+            "source": meta.get("source")
+            + " from {} to {}".format(meta.get("start"), meta.get("end")),
+        }
+    ), {"Access-Control-Allow-Origin": "*"}
 
 
-@app.get('/point/climate')
-@input(PointIn, location="query")
-@output(PointandClimateOut)
-def PointClimate(data):
-    p = Place(data['lat'], data['lon'], 0)
-    data=p.get_climate_data()
-    climatetype =data.get_koppen()
-    koppen2chinese = {
-        'Af': "热带雨林",
-        'Am': "热带季风",
-        'Aw': "热带草原",
-        'As': "热带草原",
-        'BWk': "温带干旱",
-        'BWh': "热带沙漠",
-        'BSh': "热带半干旱",
-        'BSk': "温带半干旱",
-        'Csa': "地中海",
-        'Csb': "地中海",
-        'Csc': "地中海",
-        'Cfa': "亚热带湿润",
-        'Cwa': "亚热带湿润",
-        'Cwb': "温带海洋",
-        'Cfb': "温带海洋",
-        'Cfc': "温带海洋",
-        'Dsa': "温带大陆",
-        'Dsb': "温带大陆",
-        'Dsc': "亚寒带",
-        'Dsd': "亚寒带",
-        'Dwa': "温带大陆性湿润",
-        'Dwb': "温带大陆性湿润",
-        'Dwc': "亚寒带",
-        'Dwd': "亚寒带",
-        'Dfa': "温带大陆性湿润",
-        'Dfb': "温带大陆性湿润",
-        'Dfc': "亚寒带",
-        'Dfd': "亚寒带",
-        'ET': "苔原",
-        'EF': "冰原"
-    }
-    ns = p.get_nearby_stations()
-    nsl = list()
-    if ns:
-        for i in ns:
-            nsl.append({'id': i.get_id(), 'country': i.get_country(), 'lat': i.get_place().get_lat(),
-                        'lon': i.get_place().get_lon(), 'name': i.get_name()})
-    return {
-        'country': p.get_country(),
-        'nearby_stations': nsl,
-        'data': data.get_data(),
-        'koppentype': climatetype,
-        'chinesetype': koppen2chinese.get(climatetype),
-        'source': data.get_meta().get('source')+ " from {} to {}".format(data.get_meta().get('start'), data.get_meta().get('end'))
-    }, {'Access-Control-Allow-Origin': '*'}
+@app.get("/point/climate")
+def PointClimate():
+    data = request.args
+    p = Place(data["lat"], data["lon"], 0)
+    data, meta = p.get_climate_data()
+    climatetype = data_to_koppen(data)
+    # ns = p.get_nearby_stations()
+    # nsl = list()
+    # if ns:
+    #     for i in ns:
+    #         nsl.append(
+    #             {
+    #                 "id": i.get_id(),
+    #                 "country": i.get_country(),
+    #                 "lat": i.get_place().get_lat(),
+    #                 "lon": i.get_place().get_lon(),
+    #                 "name": i.get_name(),
+    #             }
+    #         )
+    return jsonify(
+        {
+            "country": p.get_country(),
+            "nearby_stations": [],
+            "data": data,
+            "koppentype": climatetype,
+            "chinesetype": koppen2chinese.get(climatetype),
+            "source": meta.get("source")
+            + " from {} to {}".format(meta.get("start"), meta.get("end")),
+        }
+    ), {"Access-Control-Allow-Origin": "*"}
--- koppen_climate.py
+++ koppen_climate.py
@@ -2,6 +2,39 @@ import numpy as np
 import requests
 import mapbox_geocoding
 
+koppen2chinese = {
+    "Af": "热带雨林",
+    "Am": "热带季风",
+    "Aw": "热带草原",
+    "As": "热带草原",
+    "BWk": "温带干旱",
+    "BWh": "热带沙漠",
+    "BSh": "热带半干旱",
+    "BSk": "温带半干旱",
+    "Csa": "地中海",
+    "Csb": "地中海",
+    "Csc": "地中海",
+    "Cfa": "亚热带湿润",
+    "Cwa": "亚热带湿润",
+    "Cwb": "温带海洋",
+    "Cfb": "温带海洋",
+    "Cfc": "温带海洋",
+    "Dsa": "温带大陆",
+    "Dsb": "温带大陆",
+    "Dsc": "亚寒带",
+    "Dsd": "亚寒带",
+    "Dwa": "温带大陆性湿润",
+    "Dwb": "温带大陆性湿润",
+    "Dwc": "亚寒带",
+    "Dwd": "亚寒带",
+    "Dfa": "温带大陆性湿润",
+    "Dfb": "温带大陆性湿润",
+    "Dfc": "亚寒带",
+    "Dfd": "亚寒带",
+    "ET": "苔原",
+    "EF": "冰原",
+}
+
 
 def get_to_json(url, params={}):
     """
@@ -11,202 +44,170 @@ def get_to_json(url, params={}):
     :return: the json response in dict
     """
     r = requests.get(
-        url, headers={"x-api-key": "cEghsfXW7CG5W43dEzrvhjn3a8mZjkYm"}, params=params)
+        url, headers={"x-api-key": "cEghsfXW7CG5W43dEzrvhjn3a8mZjkYm"}, params=params
+    )
     if r.status_code != 200:
         raise Exception("return status" + str(r.status_code))
     else:
         return r.json()
 
 
-class ClimateData:
-    """A class of climate data
-    """
-
-    def __init__(self, data, meta):
-        """
-        A function to init the class
-        :param data:the data of the climate
-        """
-        self.__data = data
-        self.__meta = meta
-
-    def get_koppen(self):
-        """Get the climate type
-
-        :return: the köppen type of the climate(i.e. Cfa,Af .etc).
-        """
-        data = self.__data
-        avgtemp = list()
-        precip = list()
-        for i in data:
-            avgtemp.append(i['tavg'])
-            precip.append(i['prcp'])
-        avgtemp = np.array(avgtemp)
-        precip = np.array(precip)
-        totalprecip = sum(precip)
-        climate = ""
-        if min(avgtemp) >= 18.0:
-            # Tropical Rainforest
-            if min(precip) >= 60.0:
-                climate = 'Af'
-                return climate
-            # Tropical Monsoon
-            elif min(precip) < 60.0 and (min(precip) / totalprecip) > 0.04:
-                climate = 'Am'
-                return climate
-            else:
-                # Tropical Savanna Dry Summer
-                if np.where(precip == min(precip))[0][0] >= 6 and np.where(precip == min(precip))[0][0] <= 8:
-                    climate = 'As'
-                    return climate
-                # Tropical Savanna Dry Winter
-                else:
-                    climate = 'Aw'
-                    return climate
-
-        # Group B (Arid and Semiarid)
-        aridity = np.mean(avgtemp) * 20.0
-        warmprecip = sum(precip[3:9])
-        coolprecip = sum(precip[0:3]) + sum(precip[9:12])
-        if warmprecip / totalprecip >= 0.70:
-            aridity = aridity + 280.0
-        elif warmprecip / totalprecip >= 0.30 and warmprecip / totalprecip < 0.70:
-            aridity = aridity + 140.0
+def data_to_koppen(data: dict):
+    avgtemp = list()
+    precip = list()
+    for i in data:
+        avgtemp.append(i["tavg"])
+        precip.append(i["prcp"])
+    avgtemp = np.array(avgtemp)
+    precip = np.array(precip)
+    totalprecip = sum(precip)
+    if min(avgtemp) >= 18.0:
+        if min(precip) >= 60.0:
+            return "Af"
+        # Tropical Monsoon
+        elif min(precip) < 60.0 and (min(precip) / totalprecip) > 0.04:
+            return "Am"
         else:
-            aridity = aridity + 0.0
-
-        # Arid Desert (BW)
-        if totalprecip / aridity < 0.50:
-            # Hot Desert (BWh)
-            if np.mean(avgtemp) > 18.0:
-                climate = 'BWh'
-                return climate
-            # Cold Desert (BWk)
+            # Tropical Savanna Dry Summer
+            if (
+                np.where(precip == min(precip))[0][0] >= 6
+                and np.where(precip == min(precip))[0][0] <= 8
+            ):
+                return "As"
             else:
-                climate = 'BWk'
-                return climate
-
-        if 'A' in climate:
-            return climate
-
-        # Semi-Arid/Steppe (BS)
-        elif totalprecip / aridity >= 0.50 and totalprecip / aridity < 1.00:
-            # Hot Semi-Arid (BSh)
-            if np.mean(avgtemp) > 18.0:
-                climate = 'BSh'
-                return climate
-            # Cold Semi-Arid (BSk)
-            else:
-                climate = 'BSk'
-                return climate
-
-        if 'B' in climate:
-            return climate
-
-        # Group C (Temperate)
-        sortavgtemp = avgtemp
-        sortavgtemp.sort()
-        tempaboveten = np.shape(np.where(avgtemp > 10.0))[1]
-        coldwarmratio = max(max(precip[0:2]), precip[11]) / min(precip[5:8])
-        warmcoldratio = max(precip[5:8]) / min(min(precip[0:2]), precip[11])
-        if min(avgtemp) >= 0.0 and min(avgtemp) <= 18.0 and max(avgtemp) >= 10.0:
-            # Humid Subtropical (Cfa)
-            if min(avgtemp) > 0.0 and max(avgtemp) > 22.0 and tempaboveten >= 4.0:
-                climate = 'Cfa'
-            # Temperate Oceanic (Cfb)
-            elif min(avgtemp) > 0.0 and max(avgtemp) < 22.0 and tempaboveten >= 4.0:
-                climate = 'Cfb'
-            # Subpolar Oceanic (Cfc)
-            elif min(avgtemp) > 0.0 and tempaboveten >= 1 and tempaboveten <= 3:
-                climate = 'Cfc'
-
-            # Monsoon-influenced humid subtropical (Cwa)
-            if min(avgtemp) > 0.0 and max(avgtemp) > 22.0 and tempaboveten >= 4 and warmcoldratio > 10.0:
-                climate = 'Cwa'
-            # Subtropical Highland/Temperate Oceanic with Dry Winter (Cwb)
-            elif min(avgtemp) > 0.0 and max(avgtemp) < 22.0 and tempaboveten >= 4 and warmcoldratio > 10.0:
-                climate = 'Cwb'
-            # Cold Subtropical Highland/Subpolar Oceanic with Dry Winter (Cwc)
-            elif min(avgtemp) > 0.0 and tempaboveten >= 1 and tempaboveten <= 3 and warmcoldratio > 10.0:
-                climate = 'Cwc'
-
-            # Hot summer Mediterranean (Csa)
-            if min(avgtemp) > 0.0 and max(avgtemp) > 22.0 and tempaboveten >= 4 and \
-                    coldwarmratio >= 3.0 and min(precip[5:8]) < 30.0:
-                climate = 'Csa'
-            # Warm summer Mediterranean (Csb)
-            elif min(avgtemp) > 0.0 and max(avgtemp) < 22.0 and tempaboveten >= 4 and \
-                    coldwarmratio >= 3.0 and min(precip[5:8]) < 30.0:
-                climate = 'Csb'
-            # Cool summer Mediterranean (Csc)
-            elif min(avgtemp) > 0.0 and tempaboveten >= 1 and tempaboveten <= 3 and \
-                    coldwarmratio >= 3.0 and min(precip[5:8]) < 30.0:
-                climate = 'Csc'
-
-            if 'C' in climate:
-                return climate
-
-        # Group D (Continental)
-        if min(avgtemp) < 0.0 and max(avgtemp) > 10.0:
-            # Hot summer humid continental (Dfa)
-            if max(avgtemp) > 22.0 and tempaboveten >= 4:
-                climate = 'Dfa'
-            # Warm summer humid continental (Dfb)
-            elif max(avgtemp) < 22.0 and tempaboveten >= 4:
-                climate = 'Dfb'
-            # Subarctic (Dfc)
-            elif tempaboveten >= 1 and tempaboveten <= 3:
-                climate = 'Dfc'
-            # Extremely cold subarctic (Dfd)
-            elif min(avgtemp) < -38.0 and tempaboveten >= 1 and tempaboveten <= 3:
-                climate = 'Dfd'
-
-            # Monsoon-influenced hot humid continental (Dwa)
-            if max(avgtemp) > 22.0 and tempaboveten >= 4 and warmcoldratio >= 10:
-                climate = 'Dwa'
-            # Monsoon-influenced warm humid continental (Dwb)
-            elif max(avgtemp) < 22.0 and tempaboveten >= 4 and warmcoldratio >= 10:
-                climate = 'Dwb'
-            # Monsoon-influenced subarctic (Dwc)
-            elif tempaboveten >= 1 and tempaboveten <= 3 and warmcoldratio >= 10:
-                climate = 'Dwc'
-            # Monsoon-influenced extremely cold subarctic (Dwd)
-            elif min(avgtemp) < -38.0 and tempaboveten >= 1 and tempaboveten <= 3 and warmcoldratio >= 10:
-                climate = 'Dwd'
-
-            # Hot, dry continental (Dsa)
-            if max(avgtemp) > 22.0 and tempaboveten >= 4 and coldwarmratio >= 3 and min(precip[5:8]) < 30.0:
-                climate = 'Dsa'
-            # Warm, dry continental (Dsb)
-            elif max(avgtemp) < 22.0 and tempaboveten >= 4 and coldwarmratio >= 3 and min(precip[5:8]) < 30.0:
-                climate = 'Dsb'
-            # Dry, subarctic (Dsc)
-            elif tempaboveten >= 1 and tempaboveten <= 3 and coldwarmratio >= 1 and coldwarmratio >= 3 and \
-                    min(precip[5:8]) < 30.0:
-                climate = 'Dsc'
-            # Extremely cold, dry subarctic (Dsd)
-            elif min(avgtemp) < -38.0 and tempaboveten >= 1 and tempaboveten <= 3 and coldwarmratio >= 3 and \
-                    min(precip[5:8]) < 30.0:
-                climate = 'Dsd'
-
-            if 'D' in climate:
-                return climate
-
-        # Group E (Polar and alpine)
-        if max(avgtemp) < 10.0:
-            # Tundra (ET)
-            if max(avgtemp) > 0.0:
-                climate = 'ET'
-            # Ice cap (EF)
-            else:
-                climate = 'EF'
-        return climate
-
-    def get_data(self):
-        return self.__data
-
-    def get_meta(self):
-        return self.__meta
+                return "Aw"
+    aridity = np.mean(avgtemp) * 20.0
+    warmprecip = sum(precip[3:9])
+    if warmprecip / totalprecip >= 0.70:
+        aridity = aridity + 280.0
+    elif warmprecip / totalprecip >= 0.30 and warmprecip / totalprecip < 0.70:
+        aridity = aridity + 140.0
+    else:
+        aridity = aridity + 0.0
+    if totalprecip / aridity < 0.50:
+        if np.mean(avgtemp) > 18.0:
+            return "BWh"
+        else:
+            return "BWk"
+    elif totalprecip / aridity >= 0.50 and totalprecip / aridity < 1.00:
+        if np.mean(avgtemp) > 18.0:
+            return "BSh"
+        else:
+            return "BSk"
+    sortavgtemp = avgtemp
+    sortavgtemp.sort()
+    tempaboveten = np.shape(np.where(avgtemp > 10.0))[1]
+    coldwarmratio = max(max(precip[0:2]), precip[11]) / min(precip[5:8])
+    warmcoldratio = max(precip[5:8]) / min(min(precip[0:2]), precip[11])
+    if min(avgtemp) >= 0.0 and min(avgtemp) <= 18.0 and max(avgtemp) >= 10.0:
+        if min(avgtemp) > 0.0 and max(avgtemp) > 22.0 and tempaboveten >= 4.0:
+            return "Cfa"
+        elif min(avgtemp) > 0.0 and max(avgtemp) < 22.0 and tempaboveten >= 4.0:
+            return "Cfb"
+        elif min(avgtemp) > 0.0 and tempaboveten >= 1 and tempaboveten <= 3:
+            return "Cfc"
+        if (
+            min(avgtemp) > 0.0
+            and max(avgtemp) > 22.0
+            and tempaboveten >= 4
+            and warmcoldratio > 10.0
+        ):
+            return "Cwa"
+        elif (
+            min(avgtemp) > 0.0
+            and max(avgtemp) < 22.0
+            and tempaboveten >= 4
+            and warmcoldratio > 10.0
+        ):
+            return "Cwb"
+        elif (
+            min(avgtemp) > 0.0
+            and tempaboveten >= 1
+            and tempaboveten <= 3
+            and warmcoldratio > 10.0
+        ):
+            return "Cwc"
+        if (
+            min(avgtemp) > 0.0
+            and max(avgtemp) > 22.0
+            and tempaboveten >= 4
+            and coldwarmratio >= 3.0
+            and min(precip[5:8]) < 30.0
+        ):
+            return "Csa"
+        elif (
+            min(avgtemp) > 0.0
+            and max(avgtemp) < 22.0
+            and tempaboveten >= 4
+            and coldwarmratio >= 3.0
+            and min(precip[5:8]) < 30.0
+        ):
+            return "Csb"
+        elif (
+            min(avgtemp) > 0.0
+            and tempaboveten >= 1
+            and tempaboveten <= 3
+            and coldwarmratio >= 3.0
+            and min(precip[5:8]) < 30.0
+        ):
+            return "Csc"
+    if min(avgtemp) < 0.0 and max(avgtemp) > 10.0:
+        if max(avgtemp) > 22.0 and tempaboveten >= 4:
+            return "Dfa"
+        elif max(avgtemp) < 22.0 and tempaboveten >= 4:
+            return "Dfb"
+        elif tempaboveten >= 1 and tempaboveten <= 3:
+            return "Dfc"
+        elif min(avgtemp) < -38.0 and tempaboveten >= 1 and tempaboveten <= 3:
+            return "Dfd"
+        if max(avgtemp) > 22.0 and tempaboveten >= 4 and warmcoldratio >= 10:
+            return "Dwa"
+        elif max(avgtemp) < 22.0 and tempaboveten >= 4 and warmcoldratio >= 10:
+            return "Dwb"
+        elif tempaboveten >= 1 and tempaboveten <= 3 and warmcoldratio >= 10:
+            return "Dwc"
+        elif (
+            min(avgtemp) < -38.0
+            and tempaboveten >= 1
+            and tempaboveten <= 3
+            and warmcoldratio >= 10
+        ):
+            return "Dwd"
+        if (
+            max(avgtemp) > 22.0
+            and tempaboveten >= 4
+            and coldwarmratio >= 3
+            and min(precip[5:8]) < 30.0
+        ):
+            return "Dsa"
+        elif (
+            max(avgtemp) < 22.0
+            and tempaboveten >= 4
+            and coldwarmratio >= 3
+            and min(precip[5:8]) < 30.0
+        ):
+            return "Dsb"
+        elif (
+            tempaboveten >= 1
+            and tempaboveten <= 3
+            and coldwarmratio >= 1
+            and coldwarmratio >= 3
+            and min(precip[5:8]) < 30.0
+        ):
+            return "Dsc"
+        elif (
+            min(avgtemp) < -38.0
+            and tempaboveten >= 1
+            and tempaboveten <= 3
+            and coldwarmratio >= 3
+            and min(precip[5:8]) < 30.0
+        ):
+            return "Dsd"
+    if max(avgtemp) < 10.0:
+        if max(avgtemp) > 0.0:
+            return "ET"
+        else:
+            return "EF"
 
 
 class Place:
@@ -221,32 +222,40 @@ class Place:
         :param longitude: the longitude of the place
         :param elevation: the elevation of the place
         """
-        self.__latitude = latitude
-        self.__longitude = longitude
-        self.__elevation = elevation
+        self.latitude = latitude
+        self.longitude = longitude
+        self.elevation = elevation
 
-    def get_climate_data(self):  # 千万不要对Station导出的Place执行这个操作
+    def get_climate_data(self):
         """
         A function to get the climate data
         :return: the climate data in ClimateData class
         """
-        r = get_to_json("https://api.meteostat.net/v2/point/climate", params={
-            "lat": self.__latitude, "lon": self.__longitude, "alt": self.__elevation})
-        data = r['data']
-        meta = r['meta']
-        return ClimateData(data, meta)
+        r = get_to_json(
+            "https://api.meteostat.net/v2/point/climate",
+            params={
+                "lat": self.latitude,
+                "lon": self.longitude,
+                "alt": self.elevation,
+            },
+        )
+        data = r["data"]
+        meta = r["meta"]
+        return data, meta
 
     def get_nearby_stations(self):
         """The function to get nearby stations
         :return: the list of nearby stations in Station class
         """
-        data = get_to_json("https://api.meteostat.net/v2/stations/nearby",
-                           params={"lat": self.__latitude, "lon": self.__longitude})['data']
+        data = get_to_json(
+            "https://api.meteostat.net/v2/stations/nearby",
+            params={"lat": self.latitude, "lon": self.longitude},
+        )["data"]
         ans = list()
         if not data:
             return None
         for i in data:
-            id = i['id']
+            id = i["id"]
             temp = Station()
             temp.set_by_id(id)
             ans.append(temp)
@@ -257,24 +266,11 @@ class Place:
         Get the country of the place
         :return: Alpha-2 code of the country (ISO 3166)
         """
-        data = mapbox_geocoding.reverse(
-            lat=self.__latitude, lon=self.__longitude)
+        data = mapbox_geocoding.reverse(lat=self.latitude, lon=self.longitude)
         if data is None:
             return None
-        data = data.get('context')[-1]
-        return data.get('short_code')
-
-    def __repr__(self):
-        return "({},{},{})".format(self.__latitude, self.__longitude, self.__elevation)
-
-    def get_lat(self):
-        return self.__latitude
-
-    def get_lon(self):
-        return self.__longitude
-
-    def get_ele(self):
-        return self.__elevation
+        data = data.get("context")[-1]
+        return data.get("short_code")
 
 
 class Station:
@@ -287,11 +283,12 @@ class Station:
         :param data: the data
         :return: noting
         """
-        self.__id = data['id']
-        self.__name = data['name'].get('en')
-        self.__country = data['country']
-        self.__place = Place(float(data["latitude"]), float(
-            data["longitude"]), int(data["elevation"]))  # 经度纬度海拔
+        self.__id = data["id"]
+        self.__name = data["name"].get("en")
+        self.__country = data["country"]
+        self.__place = Place(
+            float(data["latitude"]), float(data["longitude"]), int(data["elevation"])
+        )  # 经度纬度海拔
 
     def set_by_id(self, ida):
         """
@@ -300,7 +297,8 @@ class Station:
         :return: noting
         """
         data = get_to_json(
-            "https://api.meteostat.net/v2/stations/meta?id={}".format(ida))['data']
+            "https://api.meteostat.net/v2/stations/meta?id={}".format(ida)
+        )["data"]
         self.set_by_data(data)
 
     def get_climate_data(self):
@@ -309,10 +307,12 @@ class Station:
         :return: the climate data in ClimateData class
         """
         data = get_to_json(
-            "https://api.meteostat.net/v2/stations/climate", params={"station": self.__id})
-        if not data['data']:
+            "https://api.meteostat.net/v2/stations/climate",
+            params={"station": self.__id},
+        )
+        if not data["data"]:
             return self.__place.get_climate_data()
-        return ClimateData(data['data'], data['meta'])
+        return data["data"], data["meta"]
 
     def get_id(self):
         return self.__id
@@ -335,8 +335,10 @@ def search_station(query, length=10):
     :return: a list of class Station
     """
     # todo : fix the bug of return 401
-    data = get_to_json("https://api.meteostat.net/v2/stations/search",
-                       params={"query": query, 'limit': length})['data']
+    data = get_to_json(
+        "https://api.meteostat.net/v2/stations/search",
+        params={"query": query, "limit": length},
+    )["data"]
     ans = list()
     for i in data:
         temp = Station()
